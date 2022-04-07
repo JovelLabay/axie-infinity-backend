5f4a658dea8d3095cca3bf3cf650261353502d0e
jovellabay <jovellabay@gmail.com>
2022-04-07 14:27:13 +0800

    Added source code for get data

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
+// ENVIRONMENT VARIABLES
+require("dotenv").config();
 
 // EXPRESS MIDDLEWARE
 const app = express();
@@ -8,9 +12,18 @@ const wallet_routes = require("./routes/wallet.routes");
 
 // PORT
 const port = process.env.PORT || 5000;
-app.listen(port, () => {
-  console.log(`The port is listening to ${port}`);
-});
+
+// DATABASE CONNECTION
+mongoose
+  .connect(process.env.MONGODB_STRING_CONNECTION)
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`The port is listening to ${port}`);
+    });
+  })
+  .catch((e) => {
+    console.log(e);
+  });
 
 // ROUTES AND ENDPOINTS
 app.use(wallet_routes);
